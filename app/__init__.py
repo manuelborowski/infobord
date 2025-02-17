80791584610cef2d11fea465cccda42b0de43596
manuel borowski <emmanuel.borowski@gmail.com>
2025-02-17 14:40:04 +0100

    0.21: small update

--- app/__init__.py
+++ app/__init__.py
@@ -34,8 +34,9 @@ from functools import wraps
 # 0.18: update on models, update, add.  When info is updated, do not create new databaseentry, but update existing.
 # 0.19: added staff field to track who add a row.  Bugfixed deleting rows
 # 0.20: extra-info, changed type to String.
+# 0.21: small update
 
-version = "0.@)"
+version = "0.21"
 
 app = Flask(__name__, instance_relative_config=True, template_folder='presentation/template/')
 
